d1181b785b4e5fd9a5f5296a8120a4caabc1ef32
root <root@localhost.localdomain>
2019-10-19 02:05:02 +0530

    documentation

--- Factorial.java
+++ Factorial.java
@@ -1,32 +1,31 @@
+// Program to find the factorial of a given number
+
 import java.util.Scanner;
 
 public class factorial {
 
 	public static void main(String[] args) {
-		
-		int num ;
-		
-		int factorial = 1 ;
-		
-		Scanner input = new Scanner(System.in) ;
-		
-		System.out.println( " Enter a number " ) ;
-		
-		num = input.nextInt() ;
-		
-		if(num <= 0)
-		{
-			System.out.println( " Enter a positive number " ) ;
+
+		int num;
+
+		int factorial = 1;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println(" Enter a number ");
+
+		num = input.nextInt();
+
+		if (num <= 0) {
+			System.out.println(" Enter a positive number ");
 		}
-				
-		else
-		{
-			for(int i = 1 ; i <= num ; i++)
-			{
-				factorial = factorial * i ;
+
+		else {
+			for (int i = 1; i <= num; i++) {
+				factorial = factorial * i;
 			}
-			
-			System.out.println(" Factorial of " + num + " : " + factorial ) ;	
+
+			System.out.println(" Factorial of " + num + " : " + factorial);
 		}
 	}
 }
